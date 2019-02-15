15918bdd51e36124782887b5d4ea538c1cfa1c76
叶立鹏 <yelipeng167@outlook.com>
2019-02-15 21:55:58 +0800

    aliens

--- 外星人入侵/game_functions.py
+++ 外星人入侵/game_functions.py
@@ -38,14 +38,20 @@ def check_events(ai_settins, screen, ship, bullets):
             check_keydown_events(event, ai_settins, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
-def update_bullets(bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """更新子弹的位置，并删除已消失的子弹"""
     # 每次循环时都重绘屏幕
     bullets.update()
     for bullet in bullets.copy():
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
-    #            print(len(bullets))
+    # 检查是否有子弹击中了外星人
+    # 如果是这样，就删除相应的子弹和外星人
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+        #            print(len(bullets))
+    if len(aliens) == 0:
+        bullets.empty
+        create_fleet(ai_settings, screen, ship, aliens)
 def fire_bullet(ai_settings, screen, ship, bullets):
     """如果还没有到达限制，就发射一颗子弹"""
     # 创建新子弹，并将其加入到编组bullets中
@@ -73,12 +79,14 @@ def create_fleet(ai_settings, screen, ship, aliens):
     """创建外星人群"""
     # 创建一个外星人，并计算每行可容纳多少个外星人
     alien = Alien(ai_settings, screen)
-
-    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+ #   number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+    number_aliens_x = int(available_space_x / (2 * alien_width))
     number_rows = get_number_rows(ai_settings, ship.rect.height,
                                   alien.rect.height)
 
-    # 创建外星人群
+    # 创建外星人
     for row_number in range(number_rows):
         for alien_number in range(number_aliens_x):
             create_alien(ai_settings, screen, aliens, alien_number,
@@ -90,3 +98,19 @@ def get_number_rows(ai_settings, ship_height, alien_height):
                            (3 * alien_height) - ship_height)
     number_rows = int(available_space_y / (2 * alien_height))
     return number_rows
+def update_aliens(ai_settings, aliens):
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
+
+def check_fleet_edges(ai_settings, aliens):
+    """有外星人到达边缘时采取相应的措施"""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+def change_fleet_direction(ai_settings, aliens):
+    """将整群外星人下移，并改变它们的方向"""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
--- 外星人入侵/settings.py
+++ 外星人入侵/settings.py
@@ -8,8 +8,14 @@ class Settings():
         self.bg_color = (230, 230, 230)
         self.ship_speed_factor = 0.5
         # 子弹设置
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 2
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
+        self.alien_speed_factor = 1
+        # 外星人设置
+        self.alien_speed_factor = 0.1
+        self.fleet_drop_speed = 1
+        # fleet_direction为1表示向右移，为-1表示向左移
+        self.fleet_direction = 1
