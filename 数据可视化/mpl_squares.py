c96d20c53dc298a7a5966c75c0d154e8308c79a5
叶立鹏 <yelipeng167@outlook.com>
2019-02-17 20:50:28 +0800

    data

---
+++ 数据可视化/mpl_squares.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+#Matplotlib是一个Python 2D绘图库，可以生成各种硬拷贝格式和跨平台的交互式环境的出版物质量数据。Matplotlib可用于Python脚本，Python和IPython shell，Jupyter笔记本，Web应用程序服务器和四个图形用户界面工具包
+squares = [1, 4, 9, 16, 25]
+plt.plot(squares)
+plt.show()
