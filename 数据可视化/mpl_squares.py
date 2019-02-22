d1ffe7930ee35149ea256d08fc2713c097b5ec3b
叶立鹏 <yelipeng167@outlook.com>
2019-02-22 08:46:56 +0800

    20190222

--- 数据可视化/mpl_squares.py
+++ 数据可视化/mpl_squares.py
@@ -1,5 +1,12 @@
 import matplotlib.pyplot as plt
 #Matplotlib是一个Python 2D绘图库，可以生成各种硬拷贝格式和跨平台的交互式环境的出版物质量数据。Matplotlib可用于Python脚本，Python和IPython shell，Jupyter笔记本，Web应用程序服务器和四个图形用户界面工具包
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+input_values = [1, 2, 3, 4, 5]
+plt.plot(input_values, squares, linewidth=5)
+# 设置图表标题，并给坐标轴加上标签
+plt.title("Square Numbers", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+# 设置刻度标记的大小
+plt.tick_params(axis='both', labelsize=14)
 plt.show()
